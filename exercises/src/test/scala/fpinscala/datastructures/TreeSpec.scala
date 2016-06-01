68197aa3d0da57b8c572f6d573c839a9ef8c79d7
Dobromir Kropielnicki <dkropielnicki@networkedassets.org>
2016-06-01 23:23:58 +0200

    Add tests for counting the size of tree

--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -1,15 +1,25 @@
 package fpinscala.datastructures
 
-import fpinscala.datastructures.Tree.size
-import org.scalatest.FunSpec
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{FunSuite , Matchers}
 
-class TreeSpec extends FunSpec {
+class TreeSpec extends FunSuite  with TableDrivenPropertyChecks with Matchers {
 
-  describe("A Tree") {
 
-    it("should have size ") {
-      val tree: Tree[Int] = Branch(Leaf(1), Leaf(2))
-      assert(size(tree) == 3)
+    test("size should count the number of nodes (leaves and branches) in a tree") {
+
+      // given
+      val trees = Table(
+        ("tree", "expectedSize"),
+        (Branch(Leaf(1), Leaf(2)), 3),
+        (Branch(Leaf(1), Branch(Leaf(2), Leaf(3))), 5),
+        (Leaf(1), 1)
+      )
+
+      // when & then
+      forAll(trees) { (tree, expectedSize) =>
+        Tree.size(tree) should be(expectedSize)
+      }
     }
-  }
+
 }
