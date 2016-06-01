cb268ca38c5528396c0fce38f4ef0afd991d127c
Dobromir Kropielnicki <dkropielnicki@networkedassets.org>
2016-06-01 23:43:16 +0200

    Solve exercise 3.26 - maximum value in the tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -12,4 +12,9 @@ object Tree {
     case Branch(l, r) => 1 + size(l) + size(r)
   }
 
+  def maximum(t: Tree[Int]): Int = t match {
+    case Leaf(n) => n
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
 }
--- exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
+++ exercises/src/test/scala/fpinscala/datastructures/TreeSpec.scala
@@ -1,25 +1,42 @@
 package fpinscala.datastructures
 
 import org.scalatest.prop.TableDrivenPropertyChecks
-import org.scalatest.{FunSuite , Matchers}
+import org.scalatest.{FunSuite, Matchers}
 
-class TreeSpec extends FunSuite  with TableDrivenPropertyChecks with Matchers {
+class TreeSpec extends FunSuite with TableDrivenPropertyChecks with Matchers {
 
 
-    test("size should count the number of nodes (leaves and branches) in a tree") {
+  test("size counts the number of nodes (leaves and branches)") {
 
-      // given
-      val trees = Table(
-        ("tree", "expectedSize"),
-        (Branch(Leaf(1), Leaf(2)), 3),
-        (Branch(Leaf(1), Branch(Leaf(2), Leaf(3))), 5),
-        (Leaf(1), 1)
-      )
+    // given
+    val trees = Table(
+      ("tree", "expectedSize"),
+      (Branch(Leaf(1), Leaf(2)), 3),
+      (Branch(Leaf(1), Branch(Leaf(2), Leaf(3))), 5),
+      (Leaf(1), 1)
+    )
 
-      // when & then
-      forAll(trees) { (tree, expectedSize) =>
-        Tree.size(tree) should be(expectedSize)
-      }
+    // when & then
+    forAll(trees) { (tree, expectedSize) =>
+      Tree.size(tree) should be(expectedSize)
     }
+  }
+
+  test("maximum returns the maximum element") {
+
+    // given
+    val trees = Table(
+      ("tree", "max"),
+      (Branch(Leaf(1), Leaf(10)), 10),
+      (Branch(Leaf(30), Branch(Leaf(2), Leaf(3))), 30),
+      (Leaf(-1), -1),
+      (Branch(Leaf(3), Branch(Leaf(-2), Leaf(3))), 3)
+    )
+
+    // when & then
+    forAll(trees) { (tree, max) =>
+      Tree.maximum(tree) should be(max)
+    }
+  }
 
 }
