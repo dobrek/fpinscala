16b379adff55577946357d4d8b55d2ed81ccea40
dobrek <dobromir.kropielnicki@gmail.com>
2016-06-21 01:21:39 +0200

    Solve exercise 5.2 - take, drop for the Stream

--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -1,4 +1,7 @@
 package fpinscala.laziness
+
+import fpinscala.laziness.Stream.cons
+
 trait Stream[+A] {
 
   def toList: List[A] = this match {
@@ -10,11 +13,11 @@ trait Stream[+A] {
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
-      case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
+      case Cons(h, t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
       case _ => z
     }
 
-  def exists(p: A => Boolean): Boolean = 
+  def exists(p: A => Boolean): Boolean =
     foldRight(false)((a, b) => p(a) || b) // Here `b` is the unevaluated recursive step that folds the tail of the stream. If `p(a)` returns `true`, `b` will never be evaluated and the computation terminates early.
 
   @annotation.tailrec
@@ -22,9 +25,16 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
 
-  def drop(n: Int): Stream[A] = sys.error("todo")
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => cons(h(), t().take(n - 1))
+    case _ => Empty
+  }
+
+  def drop(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 0 => t().drop(n - 1)
+    case _ => this
+  }
 
   def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
 
@@ -37,7 +47,9 @@ trait Stream[+A] {
 
   def startsWith[B](s: Stream[B]): Boolean = sys.error("todo")
 }
+
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -50,10 +62,11 @@ object Stream {
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if (as.isEmpty) empty 
+    if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
 
-  val ones: Stream[Int] = Stream.cons(1, ones)
+  val ones: Stream[Int] = cons(1, ones)
+
   def from(n: Int): Stream[Int] = sys.error("todo")
 
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = sys.error("todo")
--- exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
+++ exercises/src/test/scala/fpinscala/laziness/StreamSpec.scala
@@ -2,6 +2,8 @@ package fpinscala.laziness
 
 import org.scalatest.{FlatSpec, Matchers}
 
+import scala.util.Random
+
 class StreamSpec extends FlatSpec with Matchers {
 
   "toList" should "covers Stream into a List" in {
@@ -11,4 +13,31 @@ class StreamSpec extends FlatSpec with Matchers {
   it should "covers empty Stream into empty List" in {
     Stream().toList should be(List())
   }
+
+  "take" should "create a Stream from n first elements of the original once" in {
+    // given
+    val values = List(0, 1, 1, 2, 3, 5, 8, 13)
+    val n = 1 + Random.nextInt(values.length - 2)
+
+    // when & then
+    Stream(values: _*).take(n).toList should be(values.take(n))
+  }
+
+  it should "create a empty Stream from the empty one" in {
+    Stream().take(Random.nextInt(10)) should be(Empty)
+  }
+
+  it should "create a one size Stream" in {
+    Stream(2, 3, 4).take(1).toList should be(Stream(2).toList)
+  }
+
+  "drop" should "create a Stream by skipping n first elements of the original once" in {
+    // given
+    val values = List(0, 1, 1, 2, 3, 5, 8, 13)
+    val n = 1 + Random.nextInt(values.length - 2)
+
+    // when & then
+    Stream(values: _*).drop(n).toList should be(values.drop(n))
+  }
+
 }
